8130c64ea458a62171000eae507e8ca76b2a3bab
Habibullah <habibhaseeb716@gmail.com>
2024-10-02 20:15:16 +0500

    add   Al_Rehman_full_fledged_website

--- src/Components/Div.jsx
+++ src/Components/Div.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Div() {
   return (
-    <div className='container w-full h-96 bg-slate-500'>
-        <div></div>
+    <div className='division'>
+        
     </div>
   )
 }
